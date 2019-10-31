9a850339ea2e67a31030ec87823917ad28cce1d5
tidjane95 <tidjane271295@gmail.com>
2019-10-31 02:02:14 +0000

    Page d'acceuil

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestion Rendez-vous</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="Grv.css">
+
+	
+</head>
+
+
+<body id="particles-js">
+
+	
+        
+        <div class="slider">
+            <div class="slides">
+                <div class="slide"><img src="images/image1.jpg" alt="" width="300"></div>
+				<div class="slide"><img src="images/image2.jpg" alt="" width="300"></div>
+				<div class="slide"><img src="images/image3.jpg" alt="" width="300"></div>
+				<div class="slide"><img src="images/image4.jpg" alt="" width="300"></div>
+
+            </div>
+             
+            
+        </div>
+        
+           
+		<div class="contenu" >
+			<div class="souscontenu">
+                <img src="images/admin.jpg" alt="Photo administrateur" width="320px" height="240px" class="imgAdmin"><br><br><br>
+                <a href="logAdmin.php">Administrateur</a>
+
+			</div>
+			<div class="souscontenu">
+            <img src="images/medecin.jpg" alt="Photo medecin" class="imgAdmin" width="320px" height="240px"><br><br><br>
+            <a href="logMedecin.php">Medecin</a>
+
+			</div>
+			<div class="souscontenu">
+            <img src="images/secretaire.jpg" alt="Photo secretaire" class="imgAdmin" width="320px" height="240px"><br><br><br>
+            <a href="logSecreteriat.php">Secretaire</a>
+
+			</div>
+
+		</div>
+	
+    
+        <!-- <script src="js/particles.js"></script>
+        <script src="js/app.js"></script>
+        <script src="js/particles.min.js"></script> -->
+	
+</body>
+</html>
