49536cfac3077c415e8bad89483bfc5d293d06c8
tidjane95 <tidjane271295@gmail.com>
2019-10-31 02:25:24 +0000

    formulaire de suppretion des specialiter

---
+++ supprimerSpecialiter.php
@@ -0,0 +1,37 @@
+<?php
+        $serveur = "localhost";
+        $login = "root";
+        $pass="";
+        
+     try{
+         //ouverture de connexion
+         $connexion = new PDO("mysql:host=$serveur;dbname=gestionRv",$login,$pass);
+         //preparation de requete
+         $requete=$connexion->prepare("DELETE FROM specialiter   WHERE Id_Specialiter =:num LIMIT 1 ");
+         //liaison du parametre nomer
+         $requete->bindValue(':num',$_GET['Id_Specialiter'],PDO::PARAM_INT);
+          
+         //excution de la requete
+         $requete->execute(); 
+         $message = "Suppression reussit";
+         
+    
+    }
+    catch(PDOException $e){
+        echo ('Echec : '.$e->getMessage());
+    }
+ ?>   
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Supprimer medecin</title>
+ </head>
+ <body>
+     <h1> Suppression</h1>
+     <p><?= $message?></p>
+     
+ </body>
+ </html>
